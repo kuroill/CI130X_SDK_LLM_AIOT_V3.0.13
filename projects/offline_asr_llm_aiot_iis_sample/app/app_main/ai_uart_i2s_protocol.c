f8a8f3e6ee19b1e99c6cf3e6631c3db0d5738145
kuroi <yangauu@gmail.com>
2026-09-16 03:20:38 +0800

    d

--- projects/offline_asr_llm_aiot_iis_sample/app/app_main/ai_uart_i2s_protocol.c
+++ projects/offline_asr_llm_aiot_iis_sample/app/app_main/ai_uart_i2s_protocol.c
@@ -366,12 +366,9 @@ void ai_uart_i2s_handle_command(const ai_uart_i2s_command_t *cmd)
         }
         vol_set_from_esp_percent(requested_percent);
         send_ack(cmd->seq, AI_UART_ACK_OK);
-        mprintf("[AUDIO] runtime volume applied requestedPercent=%u promptRatioPercent=%u promptPcmGainPermille=%u persisted=false\r\n",
+        mprintf("[AUDIO] runtime volume applied requestedPercent=%u promptRatioPercent=60 promptPcmGainPermille=%u persisted=false\r\n",
             (unsigned int)requested_percent,
-            (unsigned int)LOCAL_PROMPT_VOLUME_RATIO_PERCENT,
-            ((unsigned int)requested_percent *
-                (unsigned int)LOCAL_PROMPT_VOLUME_RATIO_PERCENT * 1000U) /
-                (ESP_VOLUME_PCM_SCALE_DENOMINATOR * 100U));
+            ((unsigned int)requested_percent * 12U) / 5U);
         break;
     }
     case AI_UART_MSG_ENTER_WAKEUP_WAIT:
